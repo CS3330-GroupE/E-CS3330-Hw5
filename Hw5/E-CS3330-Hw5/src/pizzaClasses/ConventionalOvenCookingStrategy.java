a336fba29b54ca82bb3af6116e30e578c3fec272
Brian Bluhm <116319535+BDBluhm@users.noreply.github.com>
2024-03-12 23:43:05 -0500

    Update ConventionalOvenCookingStrategy.java
    Comment adjustments - Javadoc creation + TODO removal

--- Hw5/E-CS3330-Hw5/src/pizzaClasses/ConventionalOvenCookingStrategy.java
+++ Hw5/E-CS3330-Hw5/src/pizzaClasses/ConventionalOvenCookingStrategy.java
@@ -1,19 +1,14 @@
 package pizzaClasses;
 
 public class ConventionalOvenCookingStrategy implements ICookingStrategy {
-	//private ICookingStrategy ConventionalOvenCookingStrategy = CookingStyleType.MICROWAVE;
 
 	public ConventionalOvenCookingStrategy() {
-		// TODO Auto-generated constructor stub
-		//Must implement ICookingStrategy
 	}
 
+	/** <p> sets cooking strategy to BrickOvenCooking to pizza object, sets price to 8.00 and adds to total </p>
+	*/
 	@Override
 	public boolean cook(AbstractPizza pizza) {
-		// TODO Auto-generated method stub
-		//pizza.cookingStrategy = ConventionalOvenCookingStrategy;
-		//pizza.totalPrice = pizza.totalPrice + 8.0;
-		
 			pizza.setCookingPrice(8.0);
 			pizza.setCookingStrategy(this);
 			pizza.totalPrice += pizza.getCookingPrice();
