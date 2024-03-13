49516272dd1e57763985e15ee0b397ee06625b4e
ethanalexander <ela6m6@umsystem.edu>
2024-03-12 23:50:18 -0500

    changed comment

--- Hw5/E-CS3330-Hw5/src/main/Main.java
+++ Hw5/E-CS3330-Hw5/src/main/Main.java
@@ -6,8 +6,8 @@ import pizzaClasses.Toppings;
 
 public class Main {
 	public static void main(String[] args) {
-		// Instantiate a pizzaOrder, perform operations based on the requirements.
-		// Adds pizzas to the cart, selects cooking strategies for the pizzas in the cart,
+		/**Instantiate a pizzaOrder, perform operations based on the requirements.
+		 Adds pizzas to the cart, selects cooking strategies for the pizzas in the cart, */
 		// TODO
 		
 		PizzaOrder myOrder = new PizzaOrder();
